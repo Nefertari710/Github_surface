54fbb933ff0a8a425c83ac3c5d2931194d3e3f2c
Nefertari710 <2583741979@qq.com>
2023-03-13 05:24:20 +0800

    lab1requese

---
+++ cs732-lab-01-Nefertari710-main/exercise-02/src/App.jsx
@@ -0,0 +1,18 @@
+
+function App() {
+  return (
+    <div>
+      <h1>About me</h1>
+      {/* TODO: Add your AboutMe component here */}
+
+      <h1>My light bulb</h1>
+      {/* TODO: Add your LightBulb component here */}
+
+      <h1>My todos</h1>
+      {/* TODO: Add your todo list here */}
+
+    </div>
+  );
+}
+
+export default App;
